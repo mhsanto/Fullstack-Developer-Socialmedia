9cc79eebd944eed2e0a491a87be6d56790d7b942
Mahmodul Hasan <nicesanto92@gmail.com>
2023-12-07 17:56:53 +0600

    added tinymce editor and Tags now can be added in ui

--- components/forms/question-ask-page.tsx
+++ components/forms/question-ask-page.tsx
@@ -15,8 +15,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { formSchema } from "@/types/validations";
 
+// tinymce editor component
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Badge } from "../ui/badge";
+import { X } from "lucide-react";
+
 export default function QuestionAskSection() {
-  // ...
+  const editorRef = useRef(null);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -32,7 +38,33 @@ export default function QuestionAskSection() {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
-
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>, field: any) {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tags must be less than 15 characters",
+          });
+        }
+      }
+      if (!field.value.includes(tagValue as never)) {
+        form.setValue("tags", [...field.value, tagValue]);
+        tagInput.value = "";
+        form.clearErrors("tags");
+      } else {
+        form.trigger();
+      }
+    }
+  }
+  // remove tags from the list
+  const handleRemove = (tag: any, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
   return (
     <Form {...form}>
       <form
@@ -72,9 +104,46 @@ export default function QuestionAskSection() {
               </FormLabel>
               <FormControl className="mt-3.5">
                 {/* todo add an editor component */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_MCE_API_KEY}
+                  onInit={(evt, editor) =>
+                    // @ts-ignore
+                    (editorRef.current = editor)
+                  }
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | formatselect | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist outdent indent | " +
+                      "removeformat | help",
+                    content_style:
+                      "body { font-family:Inter,sans-serif; font-size:16px }",
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-             Introduce the problem and expand on what you put in the title,Minimum 20 characters
+                Introduce the problem and expand on what you put in the
+                title,Minimum 20 characters
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
@@ -89,11 +158,30 @@ export default function QuestionAskSection() {
                 Tags <span className="text-primary-400">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
-                  {...field}
-                  placeholder="add tags..."
-                />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                    placeholder="add tags..."
+                    onKeyDown={(e) => handleKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex gap-2 mt-2.5 flex-wrap">
+                      {field.value.map((tag) => (
+                        <Badge
+                          key={tag}
+                          className="text-dark300_light700 bg-primary-500/80 flex items-center gap-1 py-1 text-sm capitalize justify-center"
+                        >
+                          {tag}
+                          <X
+                            size={16}
+                            onClick={() => handleRemove(tag, field)}
+                            cursor="pointer"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Be specific and imagine you &apos;re asking a question to
--- components/sidebar/leftsidebar/index.tsx
+++ components/sidebar/leftsidebar/index.tsx
@@ -12,7 +12,7 @@ const LeftSidebar = () => {
     const pathname = usePathname();
 
     return (
-      <section className="flex flex-col h-full gap-3.5 ">
+      <section className="flex flex-col h-full gap-3.5 w-full">
         {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
@@ -24,8 +24,8 @@ const LeftSidebar = () => {
               className={cn(
                 isActive
                   ? "bg-blue-500/80  rounded-lg "
-                  : "bg-transparent",
-                "flex items-center text-lg justify-start gap-3 py-2 px-2.5 hover:ring-2 rounded-lg cursor-pointer "
+                  : "bg-transparent ring-0",
+                "flex items-center text-lg justify-start gap-3 py-2 ring-0 shadow-none  px-2.5 hover:ring-2 rounded-lg cursor-pointer "
               )}
             >
               <Image
@@ -55,8 +55,10 @@ const LeftSidebar = () => {
       <SignedOut>
         <div className="flex flex-col gap-3 w-full">
           <Link className="w-full" href="/sign-in">
-            <Button className="small-medium 
-            bg-blue-500  hover:bg-blue-500/30  min-h-[41px] w-full rounded-lg  py-4 shadow-none">
+            <Button
+              className="small-medium 
+              bg-blue-500/80 min-h-[41px] w-full rounded-lg  py-4 shadow-none hover:ring-1 "
+            >
               <User size={20} className="dark:invert lg:hidden" />
               <span className=" text-sm base-normal max-lg:hidden dark:text-white ">
                 Sign In
@@ -64,14 +66,16 @@ const LeftSidebar = () => {
             </Button>
           </Link>
           <Link href="/sign-up">
-            <Button className="small-medium 
+            <Button
+              className="small-medium 
             background-light800_dark400 light-border-2 btn-secondary  btn-tertiary dark:text-white
-            lg:text-dark400_light900 min-h-[41px] w-full rounded-lg  py-4 shadow-none">
+            lg:text-dark400_light900 min-h-[41px] w-full rounded-lg  py-4 shadow-none hover:ring-1 "
+            >
               <User2Icon
                 size={20}
                 className="dark:invert opacity-60 lg:hidden"
               />
-              <span className=" text-sm base-normal max-lg:hidden dark:text-white">
+              <span className=" text-sm base-normal max-lg:hidden dark:text-white ">
                 Sign up
               </span>
             </Button>
