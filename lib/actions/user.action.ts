69ed2bd0aa55b8fa39347da216340c12d69a31c1
Mahmodul Hasan <nicesanto92@gmail.com>
2023-12-14 19:37:50 +0600

    Add svix package and created api route and created route for create update delete Clerk account

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,13 +2,77 @@
 
 import User from "@/databases/user.model";
 import { connectToDatabase } from "./mongoose";
-
-export async function getUserById(params: any) {
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/databases/question.modal";
+// If the event is user.created, create the user in the database
+export async function getUserById(params: GetUserByIdParams) {
   try {
     await connectToDatabase();
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log("user.action.ts: getUserById: error: ", error);
+  }
+}
+// If the event is user.created, create the user in the database
+export async function createUser(userData: CreateUserParams) {
+  try {
+    await connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log("user.action.ts: getUserById: error: ", error);
+  }
+}
+
+// If the event is user.updated, update the user in the database
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate(
+      {
+        clerkId,
+      },
+      updateData,
+      {
+        new: true,
+      }
+    );
+    revalidatePath(path);
+  } catch (error) {
+    console.log("user.action.ts: getUserById: error: ", error);
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    const { clerkId } = params;
+    await connectToDatabase();
+    const user = await User.findOneAndDelete({
+      clerkId,
+    });
+
+    if (!user) {
+      console.error("user.action.ts: deleteUser: error: ");
+    }
+    //Delete suer from database
+    //and questions,answers, comments,etc
+    const userQUestionIds = await Question.find({ author: user._id }).distinct(
+      "_id"
+    );
+    //delete all questions
+    await Question.deleteMany({ author: user._id });
+    //TOdo delete user answers comments etc
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log("user.action.ts: getUserById: error: ", error);
   }
