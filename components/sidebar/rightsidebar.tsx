8bd090adf13b63758f8bde0cb4fcfd65278e9560
Mahmodul Hasan <nicesanto92@gmail.com>
2024-01-01 18:11:03 +0600

    created popular Tag action where it shows Popular tags in Ui Rightsidebar

--- components/sidebar/rightsidebar.tsx
+++ components/sidebar/rightsidebar.tsx
@@ -2,15 +2,12 @@ import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import Tags from "../shared/tags";
 import { getHotQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.action";
 
-const topTags = [
-  { _id: 1, tag: "javascript", questionCount: 100 },
-  { _id: 2, tag: "React", questionCount: 32 },
-  { _id: 4, tag: "Nextjs", questionCount: 30 },
-  { _id: 5, tag: "Nodejs", questionCount: 22 },
-];
+
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
+  const topTags = await getPopularTags()
 
   return (
     <div className="background-light800_darkgradient light-border custom-scrollbar text-dark300_light900 sticky right-0 top-0  flex flex-col items-start overflow-y-auto border-l p-6 max-lg:px-2 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden lg:w-[290px] ">
@@ -34,14 +31,14 @@ const RightSidebar = async () => {
       <div className="mt-16 w-full">
         <h2 className="h2-bold ">Popular Tags</h2>
         <div className="flex w-full flex-col gap-4 pt-4">
-          {topTags.map((tag) => (
+          {topTags?.map((tag) => (
             <Tags
               variant="default"
               customClasses="bg-primary-500/50
               dark:bg-primary-500/80 hover:bg-primary-500/40 py-1"
               key={tag._id}
               _id={tag._id}
-              questionCount={tag.questionCount}
+              questionCount={tag.numberOfQuestions}
               name={tag.tag}
               showCount
             />
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,8 +2,12 @@
 
 import User from "@/databases/user.model";
 import { connectToDatabase } from "./mongoose";
-import { FilterQuery } from "mongoose"
-import { GetAllTagsParams, GetQuestionsByTagIdParams, GetTopInteractedTagsParams } from "./shared.types";
+import { FilterQuery } from "mongoose";
+import {
+  GetAllTagsParams,
+  GetQuestionsByTagIdParams,
+  GetTopInteractedTagsParams,
+} from "./shared.types";
 import Tag, { ITag } from "@/databases/tag.model";
 import Question from "@/databases/question.modal";
 
@@ -38,17 +42,19 @@ export async function getAllTags(params: GetAllTagsParams) {
     console.error(` Error in getUserCreatedTags: ${error}`);
   }
 }
-
+// get questions by tagId
 export async function getQuestionByTagId(params: GetQuestionsByTagIdParams) {
   try {
     await connectToDatabase();
-    const { tagId, page = 1, pageSize = 10, searchQuery } = params
+    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
     const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
     const tag = await Tag.findOne(tagFilter).populate({
       path: "questions",
       model: Question,
-      match: searchQuery ? { title: { $regex: searchQuery, $options: "i" } } : {},
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: "i" } }
+        : {},
       options: {
         sort: { createdAt: -1 },
       },
@@ -63,5 +69,21 @@ export async function getQuestionByTagId(params: GetQuestionsByTagIdParams) {
     return { tagTitle: tag.name, questions };
   } catch (error) {
     console.error(` Error in getQuestionByTagId: ${error}`);
+  }
+}
+
+//get Popular tags
+export async function getPopularTags() {
+  try {
+    await connectToDatabase();
+    const popularTags = await Tag.aggregate([
+      { $project: { name: 1, numberofQuestions: { $size: "$questions" } } },
+      { $sort: { numberOfQuestions: -1 } },
+      { $limit: 5 },
+    ]);
+
+    return popularTags;
+  } catch (error) {
+    console.error(` Error in getPopularTags: ${error}`);
   }
 }
